94928bd5439a802f7d1275cb98e34cc34a6c99c9
Andrew Gormley <Andrew@MacBook-Pro-2.local>
2021-06-04 14:52:14 +0100

    trying to fix preview bug

--- src/js/cms-preview-templates/work.js
+++ src/js/cms-preview-templates/work.js
@@ -6,34 +6,37 @@ export default class WorkPreview extends React.Component {
     const {entry, widgetFor, getAsset} = this.props;
     let image = getAsset(entry.getIn(["data", "image"]));
 
-    return
-      <div className="animate__animated animate__fadeIn p-2">
-        <div className="border border-solid border-gray-900 dark:border-white overflow-hidden">
+    return (
+      <div className="text-sm uppercase text-gray-900 dark:text-white bg-gray-100 dark:bg-gray-900 tracking-wide transition-all">
+        <div className="animate__animated animate__fadeIn p-2">
+          <div className="border border-solid border-gray-900 dark:border-white overflow-hidden">
 
-          <div class="marquee2" data-duplicated='true' data-direction='left' data-pauseOnHover='false' data-gap='200' data-speed='100' data-startVisible='true'>
-            <span class="text-9xl font-semibold">{ entry.getIn(["data", "title"])}</span>
-          </div>
-
-          <div class="w-full flex flex-row font-semibold border-t border-b border-gray-900 dark:border-white">
-            <div class="p-5 w-1/2">
-              { format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }
+            <div class="marquee2" data-duplicated='true' data-direction='left' data-pauseOnHover='false' data-gap='200' data-speed='100' data-startVisible='true'>
+              <span class="text-9xl font-semibold">{ entry.getIn(["data", "title"])}</span>
             </div>
-            <div class="p-5 w-1/2 border-l border-solid border-gray-900 dark:border-white">
-              { entry.getIn(["data", "tags"])}
+
+            <div class="w-full flex flex-row font-semibold border-t border-b border-gray-900 dark:border-white">
+              <div class="p-5 w-1/2">
+                { format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }
+              </div>
+              <div class="p-5 w-1/2 border-l border-solid border-gray-900 dark:border-white">
+                { entry.getIn(["data", "tags"])}
+              </div>
             </div>
+
+            <div class="max-w-5xl mx-auto py-10 text-4xl normal-case p-5">
+              { entry.getIn(["data", "description"])}
+            </div>
+
+            // <div className="cms mw6">
+            //   <p>{ entry.getIn(["data", "description"]) }</p>
+            //   { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
+            //   { widgetFor("body") }
+            // </div>
+
           </div>
-
-          <div class="max-w-5xl mx-auto py-10 text-4xl normal-case p-5">
-            { entry.getIn(["data", "description"])}
-          </div>
-
-          // <div className="cms mw6">
-          //   <p>{ entry.getIn(["data", "description"]) }</p>
-          //   { image && <img src={ image } alt={ entry.getIn(["data", "title"])} /> }
-          //   { widgetFor("body") }
-          // </div>
-
         </div>
       </div>
+    );
   }
 }
