9926cc5e2edad1222ea2c5e810eb0d95ab63860e
Andrew Gormley <Andrew@MacBook-Pro-2.local>
2021-06-09 11:37:31 +0100

    more preview testing

--- src/js/cms-preview-templates/work.js
+++ src/js/cms-preview-templates/work.js
@@ -17,7 +17,7 @@ export default class WorkPreview extends React.Component {
 
             <div className="w-full flex flex-row font-semibold border-t border-b border-gray-900 dark:border-white">
               <div className="p-5 w-1/2">
-                { format(entry.getIn(["data", "date"]), "ddd, MMM D, YYYY") }
+                { format(entry.getIn(["data", "date"]), "MMM D, YYYY") }
               </div>
               <div className="p-5 w-1/2 border-l border-solid border-gray-900 dark:border-white">
                 { entry.getIn(["data", "tags"])}
@@ -28,8 +28,24 @@ export default class WorkPreview extends React.Component {
               { entry.getIn(["data", "description"])}
             </div>
 
+            <div className="px-5">
+              <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="{ entry.getIn(["data", "video"])}" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+            </div>
+
+            <div class="max-w-5xl mx-auto py-10 normal-case markdown p-5 flex flex-wrap">
+              {entry.getIn(["data", "sections"]).map( item => {
+                <div className="w-full md:w-1/2 p-2">
+                  <h4 className="font-semibold mb-2">{item.subtitle}</h4>
+                  <p>{item.copy}</p>
+                </div>
+              })}
+            </div>
+
           </div>
         </div>
+        <script type="text/javascript">
+          $('.marquee2').marquee({});
+        </script>
       </div>
     );
   }
